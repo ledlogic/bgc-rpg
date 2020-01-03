72a47de4dc2136596ed8fdbfcad27ae8bd6b6f95
Jeff Conrad <ledlogic@gmail.com>
2020-01-03 01:21:17 -0600

    Added additional life events

--- js/st/st-char.js
+++ js/st/st-char.js
@@ -8,6 +8,8 @@ st.char = {
 	worldviews: [],
 	earlybackgrounds: [],
 	childhoodevents: [],
+	friendsenemies: [],
+	lovewar: [],
 
 	init: function() {
 		st.log("init character");
@@ -18,6 +20,8 @@ st.char = {
 		that.loadWorldviews();
 		that.loadEarlybackgrounds();
 		that.loadChildhoodevents();
+		that.loadFriendsenemies();
+		that.loadLovewar();
 	},
 	reset: function() {
 		var that = st.char;
@@ -138,7 +142,35 @@ st.char = {
 				st.log(["childhoodevents",that.childhoodevents]);
 			})
 			.fail(function() {
-				alert("Error: unable to load early childhoodevents.");
+				alert("Error: unable to load early childhood events.");
+			})
+			.always(function() {
+			});
+	},
+	loadFriendsenemies: function() {
+		var that = st.char;
+		$.ajax({url: "data/friendsenemies.txt",
+				async: false})
+			.done(function(data, status, jqxhr) {
+				that.friendsenemies = data.trim().split("\n");
+				st.log(["friendsenemies",that.friendsenemies]);
+			})
+			.fail(function() {
+				alert("Error: unable to load friends and enemies.");
+			})
+			.always(function() {
+			});
+	},
+	loadLovewar: function() {
+		var that = st.char;
+		$.ajax({url: "data/lovewar.txt",
+				async: false})
+			.done(function(data, status, jqxhr) {
+				that.lovewar = data.trim().split("\n");
+				st.log(["lovewar",that.lovewar]);
+			})
+			.fail(function() {
+				alert("Error: unable to load love and war.");
 			})
 			.always(function() {
 			});
@@ -204,64 +236,99 @@ st.char = {
 	calcGoodWithTheBad: function() {
 		var that = st.char;
 		var r = st.math.dieN(10);
-		var evt = null;
+		var evt = "Good with the Bad: ";
 		switch (r) {
 			case 1:
 				var d = st.math.dieN(10);
-				evt = "Financial loss or debt: you've lost ¥" + d * 10000 + ".  If you can't play it now, you have a debt to pay, in money or blood.";
+				evt += "Financial loss or debt: you've lost ¥" + d * 10000 + ".  If you can't play it now, you have a debt to pay, in money or blood.";
 				break;
 			case 2:
-				evt = "Make a powerful connection: A local power player (warlord, official, noble, whatever) befriends yo.  Gain one free Level 3 Favor.";
+				evt += "Make a powerful connection: A local power player (warlord, official, noble, whatever) befriends yo.  Gain one free Level 3 Favor.";
 				break;
 			case 3:
 				var lvl = Math.ceil(st.math.dieN(6) / 2.0);
-				evt = "Mentor: You gained a teach or mentor in your life.  This person has taught you one new skill up to a level of " + lvl + ".";
+				evt += "Mentor: You gained a teach or mentor in your life.  This person has taught you one new skill up to a level of " + lvl + ".";
 				break;
 			case 4:
 				var d = st.math.dieN(10);
-				evt = "Imprisonment: You have been exilied, imprisoned or held hostage (your choice) for " + d + " year" + (d > 1 ? "s" : "") + ".  A good place for a PSYCHOLOGICAL complication.";
+				evt += "Imprisonment: You have been exilied, imprisoned or held hostage (your choice) for " + d + " year" + (d > 1 ? "s" : "") + ".  A good place for a PSYCHOLOGICAL complication.";
 				break;
 			case 5:
-				evt = "Falsely Accused: You were set up, and now face arrest or worse.  A good place for an ENEMY complication.";
+				evt += "Falsely Accused: You were set up, and now face arrest or worse.  A good place for an ENEMY complication.";
 				break;
 			case 6:
 				var d = st.math.dieN(10);
-				evt = "Windfall: Your financial ship just came in, ¥" + d * 10000 + ".";
+				evt += "Windfall: Your financial ship just came in, ¥" + d * 10000 + ".";
 				break;
 			case 7:
-				evt = "Accident or injury: You were in some kind of terrible accident or maimed in some other way.  A good place for a PHYSIOLOGICAL complication.";
+				evt += "Accident or injury: You were in some kind of terrible accident or maimed in some other way.  A good place for a PHYSIOLOGICAL complication.";
 				break;
 			case 8:
-				evt = "Hunted: You incurred the wrath of a powerful person, family, or group.  A good place for an ENEMY complication.";
+				evt += "Hunted: You incurred the wrath of a powerful person, family, or group.  A good place for an ENEMY complication.";
 				break;
 			case 9:
-				evt = "Mental or Physical Illness: You were struck down by a severe PHYSIOLOGICAL illness or PSYCHOLOGICAL complication.";
+				evt += "Mental or Physical Illness: You were struck down by a severe PHYSIOLOGICAL illness or PSYCHOLOGICAL complication.";
 				break;
 			case 10:
+				evt += "Emotional Loss: You lost someone you really cared about. ";
 				var d = st.math.dieN(10);
 				switch (true) {
 					case d < 4:
-						s = "They were murdered.";
+						evt += "They were murdered.";
 						break;
 					case d < 8:
-						s = "They died by accident or illness.";
+						evt += "They died by accident or illness.";
 						break;
 					default:
-						s = "They vanished, killed themselves or just up and left without any explanation.";
+						evt += "They vanished, killed themselves or just up and left without any explanation.";
 						break;
 				}
-				evt = "Emotional Loss: You lost someone you really cared about. " + s;
 				break;
 		}
 		that.addEvent(evt);
 	},
 	calcFriendsAndEnemies: function() {
 		var that = st.char;
-		var r = st.math.dieN(10);
+		var evt = "Friends & Enemies: ";
+		evt += that.friendsenemies[st.math.dieArray(that.friendsenemies)];
+		that.addEvent(evt);
 	},
 	calcLoveAndWar: function() {
 		var that = st.char;
 		var r = st.math.dieN(10);
+		var evt = "Love & War: ";
+		switch (r) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				evt += "Happy love affair: 'Nuff said.";
+				break;
+			case 5:
+			case 6:
+				evt += "Nothing serious: 'Nuff said.";
+				break;
+			default:
+				var index = st.math.dieArray(that.lovewar);
+				evt += that.lovewar[index];
+				if (index === 3) {
+					var w = st.math.dieN(3);
+					switch (w) {
+						case 1:
+							evt += " It's a boy!";
+							break;
+						case 2:
+							evt += " It's a girl!";
+							break;
+						case 2:
+							evt += " It's a baby!";
+							break;
+					}
+				}
+				st.log(["lovewar",that.spec.lovewar]);
+				break;
+		}
+		that.addEvent(evt);
 	},
 	addEvent: function(evt) {
 		var that = st.char;
@@ -279,9 +346,7 @@ st.char = {
 	renderSpec: function() {
 		st.log("rendering spec");
 		var that = st.char;
-
 		that.spec.eventsHtml = that.spec.events.join("<br/>");
-		
 		var h = "<tr><th colspan=\"2\">Lifepath Notes</th>"
 			  + "<tr><td class=\"st-tb-lbl\">Age</td><td><%- age %></td></tr>"
 			  + "<tr><td class=\"st-tb-lbl\">Values - people</td><td><%- valuesWho %></td></tr>"
@@ -289,7 +354,7 @@ st.char = {
 			  + "<tr><td class=\"st-tb-lbl\">Early Background</td><td><%- earlybackground %></td></tr>"
 			  + "<tr><td class=\"st-tb-lbl\">Personality</td><td><%- personality %></td></tr>"
 			  + "<tr><td class=\"st-tb-lbl\">Childhood Event</td><td><%- childhoodevent %></td></tr>"
-			  + "<tr><td class=\"st-tb-lbl\">Life Event</td><td><%= eventsHtml %></td></tr>"
+			  + "<tr><td class=\"st-tb-lbl\">Life Event(s)</td><td><%= eventsHtml %></td></tr>"
 		;
 		var template = _.template(h);
 		var t = "<table class=\"st-tb\"><tbody>" + template(that.spec) + "</tbody></table>";		
